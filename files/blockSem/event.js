313811cf55734470d078fa0953cda8acfdd4661b
Andrey Shi <andy.roshitu@gmail.com>
2023-11-27 12:58:14 +0300

    upgrade sem event data structure

--- files/blockSem/event.js
+++ files/blockSem/event.js
@@ -1,19 +1,26 @@
 const {stations} = require("./station-name");
 const dayjs = require("dayjs");
+function generateValue() {
+    return {
+        value: Math.random().toFixed(4),
+        date: dayjs().subtract(Math.floor(Math.random() * 2), 'hour').format('YYYY-MM-DD HH:mm')
+    }
+}
+
+
 module.exports = {
     generateSemEvents() {
         return stations.map((station) => ({
             stationName: station,
-            date: dayjs().subtract(Math.floor(Math.random() * 2), 'hour').format('YYYY-MM-DD HH:mm'),
-            hydrogenSulfide: Math.random().toFixed(4),
-            sulfurOxide: Math.random().toFixed(4),
-            nitricDioxide: Math.random().toFixed(4),
-            nitricMonoxide: Math.random().toFixed(4),
-            nitricOxide: Math.random().toFixed(4),
-            methane: Math.random().toFixed(4),
-            tetrahydrocannabinol: Math.random().toFixed(4),
-            CHx: Math.random().toFixed(4),
-            carbonMonoxide: Math.random().toFixed(4),
+            hydrogenSulfide: generateValue(),
+            sulfurOxide: generateValue(),
+            nitricDioxide: generateValue(),
+            nitricMonoxide: generateValue(),
+            nitricOxide: generateValue(),
+            methane: generateValue(),
+            tetrahydrocannabinol: generateValue(),
+            CHx: generateValue(),
+            carbonMonoxide: generateValue(),
             temperature: 20,
             windDirection: Math.floor(Math.random() * 360),
             windSpeed: (Math.random() * 10).toFixed(2),
