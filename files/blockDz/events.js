e7f855e55bf92e825c51b34c2269ca7edc5c8a41
andy-rosa <andy.roshitu@gmail.com>
2023-11-30 02:15:45 +0300

    feat: add coordinates

--- files/blockDz/events.js
+++ files/blockDz/events.js
@@ -5,7 +5,7 @@ module.exports = {
         const bushes = ["АГЗУ-1", "АГЗУ-2", "АГЗУ-3", "ЗУ-3", "ЗУ-4", "ЗУ-5", "ЗУ-6"];
         const possibleEvents = ['Сработал датчик CN₄', 'Сработал датчик H₂S', 'Сработал датчик CH4O'];
         const electricalNetworks = ['разв07', 'разв09', 'пол15']
-        const randomStatus = Math.floor(Math.random() * 6)
+        const randomStatus = Math.round(Math.random() * 6)
 
         return {
             id: Date.now().toString(36) + Math.random().toString(36).substring(2),
@@ -19,8 +19,9 @@ module.exports = {
             sensorValue: Math.floor(1 + Math.random() * (10)),
             eventStatus: Math.floor(Math.random() * 2) + 2,
             distance: Math.floor(Math.random() * 10),
-            status: randomStatus === 2 ? 0 : randomStatus,
-            electricalNetwork: electricalNetworks[Math.floor(Math.random() * 3)]
+            status: randomStatus,
+            electricalNetwork: electricalNetworks[Math.floor(Math.random() * 3)],
+            coordinates: [Math.floor(Math.random() * 100), Math.floor(Math.random() * 100)]
         };
     },
 };
